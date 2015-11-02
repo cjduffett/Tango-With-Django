4d889ce535978d95742f6ddfbf54bd58f13e8242
Carlton Duffett <cjduffett@comcast.net>
2015-11-02 00:35:36 -0500

    Added About Page

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    return HttpResponse("<h1>Rango says hey there world!</h1> <a href='/rango/about'>About</a>")
+
+def about(request):
+    return HttpResponse("<h1>Rango says here is the about page.</h1> <a href='/rango/'>Index</a>")
