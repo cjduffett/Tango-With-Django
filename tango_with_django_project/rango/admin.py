a971f38ed38a772f34e15e9de7bdc3e0563b00b8
Carlton Duffett <cjduffett@comcast.net>
2015-11-06 19:48:40 -0500

    Added models, admin site customization

--- tango_with_django_project/rango/admin.py
+++ tango_with_django_project/rango/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from rango.models import Category, Page
 
-# Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    fields = ['name', 'views', 'likes']
+    list_display = ('name', 'views', 'likes')
+    search_fields = ['name']
+
+admin.site.register(Category, CategoryAdmin)
+
+class PageAdmin(admin.ModelAdmin):
+    fields = ['category', 'title', 'url', 'views']
+    list_display = ('category', 'title', 'url')
+    search_fields = ['title']
+
+admin.site.register(Page, PageAdmin)
